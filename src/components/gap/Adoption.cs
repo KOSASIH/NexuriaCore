67466e09682cb5a5a54d26329e3ee00ea08e9218
KOSASIH <kosasihg88@gmail.com>
2024-09-24 13:07:00 +0700

    Create Adoption.cs

---
+++ src/components/gap/Adoption.cs
@@ -0,0 +1,10 @@
+namespace Nexuria.GAP.Models
+{
+    public class Adoption
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
